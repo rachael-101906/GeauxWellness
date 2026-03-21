d076633a11b3c1b94e38cb836c8b52a163d82fae
rachael-101906 <rachaelbergeron@icloud.com>
2026-03-21 13:41:52 -0500

    add/quixemojis

--- geauxwellness/firebase.config.js
+++ geauxwellness/firebase.config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Analytics is only available in browser environments.
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+
+export { app, db, analytics };
---
+++ geauxwellness/src/constants/images.js
@@ -0,0 +1,13 @@
+import happy2 from '../assets/happy2.png'
+import angry from '../assets/angry.png'
+import hungry from '../assets/hungry.png'
+import nervous from '../assets/nervous.png'
+import sad from '../assets/sad.png'
+
+export const images = {
+  happyEmoji: happy2,
+  angryEmoji: angry,
+  hungryEmoji: hungry,
+  nervousEmoji: nervous,
+  sadEmoji: sad
+}
